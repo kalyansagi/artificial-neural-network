4a792b36fa63fe8f29bde6f74a41eec671f8cf60
Venkata Kalyan Sagi <kalyansagi.555@gmail.com>
2022-06-19 09:26:53 -0400

    next step of working model. Not able to visualize the results yet

--- src/Prog_Assi_02.py
+++ src/Prog_Assi_02.py
@@ -4,20 +4,31 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('.../Bank_Predictions.csv')
+dataset = pd.read_csv('Bank_Predictions.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
+print("content in X", X)
+print("content in y", y)
 # ------ Part-1: Data preprocessing ----------
 
 # Encoding categorical data
+# from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# labelencoder_X_1 = LabelEncoder()
+# X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+# labelencoder_X_2 = LabelEncoder()
+# X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+# onehotencoder = OneHotEncoder(categorical_features=[1])
+# X = onehotencoder.fit_transform(X).toarray()
+# X = X[:, 1:]
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
 labelencoder_X_2 = LabelEncoder()
 X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-onehotencoder = OneHotEncoder(categorical_features=[1])
-X = onehotencoder.fit_transform(X).toarray()
+ct1 = ColumnTransformer([("Geography", OneHotEncoder(), [1])], remainder='passthrough')
+X = ct1.fit_transform(X)
 X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
@@ -33,6 +44,7 @@ X_test = sc_X.transform(X_test)
 # ------- Part-2: Build the ANN --------
 
 # import keras library and packages
+# Note: I had to manually install tensorflow before importing the below libraries. pip3 install tensorflow
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
@@ -43,22 +55,23 @@ classifier = Sequential()
 # You might use the following parameters: activation: 'relu' and/or 'sigmoid', optimization function is 'adam', loss function is 'binary_crossentropy', number of epochs is 100, samples per epoch are 10) 
 
 # Adding the input layer and the first hidden layer
-# The missing line of code here....
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
 
 # Adding second hidden layer
-# The missing line of code here....
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
 
 # Adding output layer
-# The missing line of code here....
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
 
 # Compiling the ANN
-# The missing line of code here....
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 # Fitting the ANN to the training set
-# The missing line of code here....
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
 
 # Predicting the Test set results
-# The missing line of code here....
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
 
 # Making the confusion Matrix
 from sklearn.metrics import confusion_matrix
